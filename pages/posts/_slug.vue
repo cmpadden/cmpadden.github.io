8e1d0b56375ab3c44e15827a07df49c17913b5e6
Colton Padden <colton.padden@fastmail.com>
2021-12-28 09:35:01 -0500

    increase post font size

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -2,14 +2,14 @@
   <div class="container mx-auto px-2">
     <div class="mx-6 md:mx-12">
       <article class="shadow-lg bg-gray-200 mb-10 p-10">
-        <h1 class="font-bold sm:text-2xl md:text-3xl lg:text-4xl text-gray-700">
+        <h1 class="font-bold text-3xl text-gray-700">
           {{ article.title }}
         </h1>
-        <p class="sm:text-xs text-sm text-gray-600 flex items-center mb-4">
+        <p class="text-sm text-gray-600 flex items-center mb-4">
           {{ formatDate(article.updatedAt) }}
         </p>
         <!-- https://github.com/tailwindlabs/tailwindcss-typography#overriding-max-width -->
-        <article class="prose-sm md:prose max-w-none">
+        <article class="prose max-w-none">
           <nuxt-content :document="article" />
         </article>
       </article>
