5ed8175a269a617c8c364d038a564a2fa9226d09
Colton Padden <colton.padden@fastmail.com>
2022-01-10 16:07:33 -0500

    update website title and package name

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-blog',
+    title: 'Colton Padden\'s Personal Website',
     htmlAttrs: {
       lang: 'en',
     },
