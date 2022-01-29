0892b2ed1606d6e005fb3c427906a4baf79fee48
Colton Padden <colton.padden@fastmail.com>
2022-01-29 10:25:18 -0500

    playground: allow variable manipulation

--- pages/playground/p5-demo/index.vue
+++ pages/playground/p5-demo/index.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="bg-gradient-to-b from-green-800 to-gray-800">
     <div class="h-screen grid place-items-center">
-      <div id="canvas" class="border-2 border-white" />
+      <div class="font-mono text-white">
+        <div class="items-end my-2 md:flex">
+          <div class="flex-none md:flex-1">y(x) = A sin((2π / λ) x)</div>
+          <div>
+            <label for="aInput" class="inline-block form-label">A</label>
+            <input
+              id="aInput"
+              type="number"
+              class="w-16 p-2 bg-transparent border-2 border-white focus:outline-none"
+              v-model.number="wave.amplitude"
+            />
+            <label for="lInput" class="inline-block form-label">λ</label>
+            <input
+              id="lInput"
+              type="number"
+              class="w-16 p-2 bg-transparent border-2 border-white focus:outline-none"
+              v-model.number="wave.lambda"
+            />
+            <label for="dInput" class="inline-block form-label">◒</label>
+            <input
+              id="dInput"
+              type="number"
+              class="w-16 p-2 bg-transparent border-2 border-white focus:outline-none"
+              v-model.number="ball.diameter"
+            />
+          </div>
+        </div>
+
+        <div id="canvas" class="border-2 border-white"></div>
+        <div class="w-48 my-2 md:w-full">
+          Click or tap anywhere to clear the canvas!
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,51 +45,56 @@ export default Vue.extend({
   layout: 'light',
   mounted() {
     const sketch = (s) => {
-      const HEIGHT = s.windowWidth >= 400 ? 400 : 200
-      const WIDTH = s.windowWidth >= 400 ? 800 : 200
+      const HEIGHT = s.windowWidth >= 450 ? 400 : 300
+      const WIDTH = s.windowWidth >= 450 ? 800 : 300
 
-      const AMPLITUDE = HEIGHT / 4
-      const BALLWIDTH = HEIGHT / 6
-      const BALLHEIGHT = HEIGHT / 6
-      const LAMBDA = WIDTH / 3
-
-      let ballX = -BALLWIDTH
-      let ballY = HEIGHT / 2
-      let R = s.random(255)
-      let G = s.random(255)
-      let B = s.random(255)
+      let x = 0
+      let y = HEIGHT / 2
 
       s.setup = () => {
-        // s.createCanvas(s.windowWidth, s.windowHeight)
         s.createCanvas(WIDTH, HEIGHT)
+        this.ball.color.R = Math.round(s.random(255))
+        this.ball.color.G = Math.round(s.random(255))
+        this.ball.color.B = Math.round(s.random(255))
+        this.ball.diameter = Math.round(HEIGHT / 6)
+        x = -this.ball.diameter
+
+        this.wave.amplitude = Math.round(HEIGHT / 4)
+        this.wave.lambda = Math.round(WIDTH / 3)
       }
 
       s.draw = () => {
         s.noStroke()
 
-        s.fill(s.color(R, G, B))
-        s.ellipse(ballX, ballY, BALLWIDTH, BALLHEIGHT)
+        s.fill(s.color(this.ball.color.R, this.ball.color.G, this.ball.color.B))
+        s.ellipse(x, y, this.ball.diameter, this.ball.diameter)
 
-        // ballY = 150 * s.sin(ballX / 50) + s.windowHeight / 2
-        ballY = AMPLITUDE * s.sin(ballX * (s.TWO_PI / LAMBDA)) + HEIGHT / 2
+        y =
+          this.wave.amplitude * s.sin(x * (s.TWO_PI / this.wave.lambda)) +
+          HEIGHT / 2
 
         // compute R color channel based on ball's Y position
-        R = s.map(
-          ballY,
-          -AMPLITUDE + HEIGHT / 2,
-          AMPLITUDE + HEIGHT / 2,
-          0,
-          255
+        this.ball.color.R = Math.round(
+          s.map(
+            y,
+            -this.wave.amplitude + HEIGHT / 2,
+            this.wave.amplitude + HEIGHT / 2,
+            0,
+            255
+          )
         )
 
-        // ballX = (ballX + 1) % s.windowWidth
-        ballX = ballX >= WIDTH + BALLWIDTH ? -BALLWIDTH : ballX + 1
+        x = x >= WIDTH + this.ball.diameter ? -this.ball.diameter : x + 1
       }
 
+      // s.mousePressed = () => {
+      //   this.ball.color.R = s.random(255)
+      //   this.ball.color.G = s.random(255)
+      //   this.ball.color.B = s.random(255)
+      // }
+
       s.mousePressed = () => {
-        R = s.random(255)
-        G = s.random(255)
-        B = s.random(255)
+        s.clear()
       }
 
       // s.windowResized = () => {
@@ -69,7 +106,20 @@ export default Vue.extend({
     new this.$p5(sketch, 'canvas')
   },
   data() {
-    return {}
+    return {
+      wave: {
+        amplitude: 0,
+        lambda: 0,
+      },
+      ball: {
+        diameter: 0,
+        color: {
+          R: 0,
+          G: 0,
+          B: 0,
+        },
+      },
+    }
   },
 })
 </script>
