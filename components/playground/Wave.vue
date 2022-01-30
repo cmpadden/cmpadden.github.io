d86f964767ad99b37d74d817e3dfa39e95e7ae26
Colton Padden <colton.padden@fastmail.com>
2022-01-30 10:55:21 -0500

    playground: refactor wave sketch to component

---
+++ components/playground/Wave.vue
@@ -0,0 +1,101 @@
+<template>
+  <!-- canvas requires a unique ID for rendering -->
+  <div :id="id" />
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    id: {
+      type: String,
+      default: 'canvas',
+    },
+    canvasHeight: {
+      type: Number,
+      default: 250,
+    },
+    type: {
+      type: String,
+      default: 'sin', // sin, cos, tan
+    },
+    diameter: {
+      type: Number,
+      default: 15,
+    },
+    amplitude: {
+      type: Number,
+      default: 75,
+    },
+    lambda: {
+      type: Number,
+      default: 266,
+    },
+  },
+  mounted() {
+    const sketch = (s) => {
+      const HEIGHT = s.windowWidth >= 450 ? this.canvasHeight : 200
+      const WIDTH = s.windowWidth >= 450 ? 800 : 300
+
+      let x = 0
+      let y = HEIGHT / 2
+
+      // let R = Math.round(s.random(255))
+      // let G = Math.round(s.random(255))
+      // let B = Math.round(s.random(255))
+
+      let R = 0
+      const G = 100
+      const B = 125
+
+      s.setup = () => {
+        s.createCanvas(WIDTH, HEIGHT)
+        x = -this.diameter
+      }
+
+      s.draw = () => {
+        if (this.type === 'sin') {
+          y = this.amplitude * s.sin(x * (s.TWO_PI / this.lambda)) + HEIGHT / 2
+        } else if (this.type === 'cos') {
+          y = this.amplitude * s.cos(x * (s.TWO_PI / this.lambda)) + HEIGHT / 2
+        } else if (this.type === 'tan') {
+          y = this.amplitude * s.tan(x * (s.TWO_PI / this.lambda)) + HEIGHT / 2
+        } else {
+          // unsupported wave type
+          y = HEIGHT / 2
+        }
+
+        // compute R color channel based on ball's Y position
+        R = Math.round(
+          s.map(
+            y,
+            -this.amplitude + HEIGHT / 2,
+            this.amplitude + HEIGHT / 2,
+            0,
+            255
+          )
+        )
+
+        x = x >= WIDTH + this.diameter ? -this.diameter : x + 1
+
+        s.noStroke()
+        s.fill(s.color(R, G, B))
+        s.ellipse(x, y, this.diameter, this.diameter)
+      }
+
+      s.mousePressed = () => {
+        s.clear()
+      }
+    }
+
+    // eslint-disable-next-line no-new
+    new this.$p5(sketch, this.id)
+  },
+  data() {
+    return {}
+  },
+})
+</script>
+
+
--- pages/playground/sine/index.vue
+++ pages/playground/sine/index.vue
@@ -4,32 +4,59 @@
       <div class="font-mono text-white">
         <div class="items-end my-2 md:flex">
           <div class="flex-none md:flex-1">y(x) = A sin((2π / λ) x)</div>
-          <div>
+          <div class="hidden md:block">
             <label for="aInput" class="inline-block form-label">A</label>
             <input
               id="aInput"
+              v-model.number="wave.amplitude"
               type="number"
               class="w-16 p-2 bg-transparent border-2 border-white focus:outline-none"
-              v-model.number="wave.amplitude"
             />
             <label for="lInput" class="inline-block form-label">λ</label>
             <input
               id="lInput"
+              v-model.number="wave.lambda"
               type="number"
               class="w-16 p-2 bg-transparent border-2 border-white focus:outline-none"
-              v-model.number="wave.lambda"
             />
             <label for="dInput" class="inline-block form-label">◒</label>
             <input
               id="dInput"
+              v-model.number="wave.diameter"
               type="number"
               class="w-16 p-2 bg-transparent border-2 border-white focus:outline-none"
-              v-model.number="ball.diameter"
             />
           </div>
         </div>
-
-        <div id="canvas" class="border-2 border-white"></div>
+        <div class="mb-2 border-2 border-white">
+          <PlaygroundWave
+            id="canvas1"
+            type="sin"
+            :diameter="wave.diameter"
+            :amplitude="wave.amplitude"
+            :lambda="wave.lambda"
+          />
+        </div>
+        <div>y(x) = A cos((2π / λ) x)</div>
+        <div class="mb-2 border-2 border-white">
+          <PlaygroundWave
+            id="canvas2"
+            type="cos"
+            :diameter="wave.diameter"
+            :amplitude="wave.amplitude"
+            :lambda="wave.lambda"
+          />
+        </div>
+        <div>y(x) = A tan((2π / λ) x)</div>
+        <div class="border-2 border-white">
+          <PlaygroundWave
+            id="canvas3"
+            type="tan"
+            :diameter="wave.diameter"
+            :amplitude="wave.amplitude"
+            :lambda="wave.lambda"
+          />
+        </div>
         <div class="w-48 my-2 md:w-full">
           Click or tap anywhere to clear the canvas!
         </div>
@@ -43,81 +70,12 @@ import Vue from 'vue'
 
 export default Vue.extend({
   layout: 'light',
-  mounted() {
-    const sketch = (s) => {
-      const HEIGHT = s.windowWidth >= 450 ? 400 : 300
-      const WIDTH = s.windowWidth >= 450 ? 800 : 300
-
-      let x = 0
-      let y = HEIGHT / 2
-
-      s.setup = () => {
-        s.createCanvas(WIDTH, HEIGHT)
-        this.ball.color.R = Math.round(s.random(255))
-        this.ball.color.G = Math.round(s.random(255))
-        this.ball.color.B = Math.round(s.random(255))
-        this.ball.diameter = Math.round(HEIGHT / 6)
-        x = -this.ball.diameter
-
-        this.wave.amplitude = Math.round(HEIGHT / 4)
-        this.wave.lambda = Math.round(WIDTH / 3)
-      }
-
-      s.draw = () => {
-        s.noStroke()
-
-        s.fill(s.color(this.ball.color.R, this.ball.color.G, this.ball.color.B))
-        s.ellipse(x, y, this.ball.diameter, this.ball.diameter)
-
-        y =
-          this.wave.amplitude * s.sin(x * (s.TWO_PI / this.wave.lambda)) +
-          HEIGHT / 2
-
-        // compute R color channel based on ball's Y position
-        this.ball.color.R = Math.round(
-          s.map(
-            y,
-            -this.wave.amplitude + HEIGHT / 2,
-            this.wave.amplitude + HEIGHT / 2,
-            0,
-            255
-          )
-        )
-
-        x = x >= WIDTH + this.ball.diameter ? -this.ball.diameter : x + 1
-      }
-
-      // s.mousePressed = () => {
-      //   this.ball.color.R = s.random(255)
-      //   this.ball.color.G = s.random(255)
-      //   this.ball.color.B = s.random(255)
-      // }
-
-      s.mousePressed = () => {
-        s.clear()
-      }
-
-      // s.windowResized = () => {
-      //   s.resizeCanvas(s.windowWidth, s.windowHeight)
-      // }
-    }
-
-    // eslint-disable-next-line no-new
-    new this.$p5(sketch, 'canvas')
-  },
   data() {
     return {
       wave: {
-        amplitude: 0,
-        lambda: 0,
-      },
-      ball: {
-        diameter: 0,
-        color: {
-          R: 0,
-          G: 0,
-          B: 0,
-        },
+        amplitude: 75,
+        lambda: 266,
+        diameter: 15,
       },
     }
   },
