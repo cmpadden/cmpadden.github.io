48da4c8c7339efe125eeb4ce739d15106d6c8c69
Colton Padden <colton.padden@fastmail.com>
2023-05-27 09:49:34 -0400

    mesh gradient

--- components/SectionInterests.vue
+++ components/SectionInterests.vue
@@ -1,7 +1,5 @@
 <template>
-  <section
-    class="py-2 md:py-8 text-white bg-gradient-to-r from-blackish via-blue-400 to-green-400"
-  >
+  <section class="py-2 md:py-8 text-white bg-mesh-gradient">
     <div class="container mx-auto">
       <div
         class="grid grid-cols-1 space-y-2 md:grid-cols-3 md:space-x-2 md:space-y-0"
@@ -63,3 +61,28 @@
     </div>
   </section>
 </template>
+
+<style>
+/* https://hypercolor.dev/mesh
+ *
+ * bg-teal-700 2 33 58
+ * bg-sky-700 3 100 80
+ * bg-neutral-600 6 58 12
+ * bg-green-900 48 37 92
+ * bg-slate-300 88 74 62
+ * bg-teal-100 67 90 81
+ */
+.bg-mesh-gradient {
+  background-color: rgb(82, 82, 91);
+  background-image: radial-gradient(
+      at 2% 33%,
+      rgb(15, 118, 110) 0,
+      transparent 58%
+    ),
+    radial-gradient(at 3% 100%, rgb(3, 105, 161) 0, transparent 80%),
+    radial-gradient(at 6% 58%, rgb(82, 82, 82) 0, transparent 12%),
+    radial-gradient(at 48% 37%, rgb(20, 83, 45) 0, transparent 92%),
+    radial-gradient(at 88% 74%, rgb(203, 213, 225) 0, transparent 62%),
+    radial-gradient(at 67% 90%, rgb(204, 251, 241) 0, transparent 81%);
+}
+</style>
