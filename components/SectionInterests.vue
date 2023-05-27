4fc25dc528aabb322236cee8acb1e15767af99ba
Colton Padden <colton.padden@fastmail.com>
2023-05-27 09:31:02 -0400

    layout interests

--- components/SectionInterests.vue
+++ components/SectionInterests.vue
@@ -1,14 +1,21 @@
 <template>
-  <section class="py-2 md:py-8 text-white bg-gradient-to-r from-blackish via-blue-400 to-green-400">
+  <section
+    class="py-2 md:py-8 text-white bg-gradient-to-r from-blackish via-blue-400 to-green-400"
+  >
     <div class="container mx-auto">
-      <div class="grid grid-cols-1 space-y-2 md:grid-cols-3 md:space-x-2 md:space-y-0">
+      <div
+        class="grid grid-cols-1 space-y-2 md:grid-cols-3 md:space-x-2 md:space-y-0"
+      >
         <div>
-          <div class="h-full p-3 bg-black border bg-opacity-70">
-            <h3 class="pb-2 text-xl font-bold text-center">
+          <div
+            class="h-full bg-black border-2 border-white bg-opacity-70 rounded-lg drop-shadow-lg"
+          >
+            <h3
+              class="p-2 text-xl font-bold text-center border-b-2 border-white"
+            >
               Technical Interests
             </h3>
-            <hr class="py-2 border-gray-300" />
-            <ul class="text-base custom-list">
+            <ul class="text-base custom-list p-2">
               <li>Data Pipeline Engineering</li>
               <li>Distributed Systems</li>
               <li>Embedded Systems</li>
@@ -18,10 +25,15 @@
           </div>
         </div>
         <div>
-          <div class="h-full p-3 bg-black border bg-opacity-70">
-            <h3 class="pb-2 text-xl font-bold text-center">Air Quality</h3>
-            <hr class="py-2 border-gray-300" />
-            <p class="mb-8 text-base text-center">
+          <div
+            class="h-full bg-black border-2 border-white bg-opacity-70 rounded-lg drop-shadow-lg"
+          >
+            <h3
+              class="p-2 text-xl font-bold text-center border-b-2 border-white"
+            >
+              Air Quality
+            </h3>
+            <p class="p-2 text-base text-center">
               I am a founding member and technical adviser to
               <a class="underline" href="https://guaq.tech"
                 >Globally Unified Air Quality</a
@@ -31,10 +43,15 @@
           </div>
         </div>
         <div>
-          <div class="h-full p-3 bg-black border bg-opacity-70">
-            <h3 class="pb-2 text-xl font-bold text-center">Consulting</h3>
-            <hr class="py-2 border-gray-300" />
-            <p class="mb-8 text-base text-center">
+          <div
+            class="h-full bg-black border-2 border-white bg-opacity-70 rounded-lg drop-shadow-lg"
+          >
+            <h3
+              class="p-2 text-xl font-bold text-center border-b-2 border-white"
+            >
+              Consulting
+            </h3>
+            <p class="p-2 text-base text-center">
               I worked as a technical consultant for financial institutions and
               government agencies in the Washington, D.C. area, providing
               guidance on identity and access management, and big data
