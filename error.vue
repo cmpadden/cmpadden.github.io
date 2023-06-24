5142f14d86d1686d3c06d460f1af4639dc38c8f2
Colton Padden <colton.padden@fastmail.com>
2023-06-24 16:58:09 -0400

    simplify 404; remove randy jackson (lol)

--- error.vue
+++ error.vue
@@ -1,23 +1,30 @@
 <template>
-  <body class="flex flex-col min-h-screen bg-grayish font-display">
+  <body class="flex flex-col min-h-screen font-display bg-background">
     <Header />
     <main class="flex-1 mt-4">
       <div class="flex justify-center">
-        <div class="rounded shadow-lg p-6 text-center">
-          <img
-            class="object-scale-down h-48 w-full"
-            src="/images/randy-jackson.png"
-          />
-          <div class="font-weight-medium italic text-xl text-white">
-            That's a yikes from me, dawg.
+        <HeadlessTransitionRoot
+          show
+          appear
+          as="template"
+          enter="transition transform duration-300 ease-out"
+          enter-from="translate-x-8 opacity-0"
+          enter-to="translate-x-0 opacity-100"
+          leave="transition transform duration-300 ease-in"
+          leave-from="opacity-100"
+          leave-to="-translate-x-8 opacity-0"
+        >
+          <div class="bg-black/50 rounded-xl shadow-lg p-6 text-center">
+            <div class="text-6xl text-white font-extrabold tracking-widest">
+              Yikes!
+            </div>
+            <div class="text-base text-gray-200">
+              Either the page you're looking for doesn't exist, or something has
+              gone terribly wrong.
+            </div>
           </div>
-          <div class="text-base text-gray-200">
-            Either the page you're looking for doesn't exist, or something has
-            gone terribly wrong.
-          </div>
-        </div>
+        </HeadlessTransitionRoot>
       </div>
     </main>
-    <Footer />
   </body>
 </template>
