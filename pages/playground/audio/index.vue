5f3a93288e7c350bdcd14a4df399dd493149a1a5
Colton Padden <colton.padden@fastmail.com>
2023-06-24 20:52:34 -0400

    temporary workaround for mobile view of microphone analysis

--- pages/playground/audio/index.vue
+++ pages/playground/audio/index.vue
@@ -1,70 +1,79 @@
 <template>
-  <div class="font-mono text-white bg-gradient-to-b from-red-900 to-gray-900">
-    <div class="h-screen grid place-items-center">
-      <!-- <div>Raw Data Buffer</div> -->
-      <!-- <div class="mb-2 border-2 border-white"> -->
-      <!--   <div v-for="(d, ix) in dataArrayHistory" :key="ix">{{ d }}</div> -->
-      <!-- </div> -->
-      <div>
-        <div>Waveform</div>
-        <div class="mb-2 border-2 border-white">
-          <PlaygroundAudioWaveform :canvas-height="150" />
+    <div class="font-mono text-white bg-gradient-to-b from-red-900 to-gray-900">
+        <div class="grid place-items-center p-4">
+            <!-- <div>Raw Data Buffer</div> -->
+            <!-- <div class="mb-2 border-2 border-white"> -->
+            <!--   <div v-for="(d, ix) in dataArrayHistory" :key="ix">{{ d }}</div> -->
+            <!-- </div> -->
+            <div>
+                <div>Waveform</div>
+                <div class="mb-2 border-2 border-white">
+                    <PlaygroundAudioWaveform :canvas-height="150" :canvas-width="canvasWidth" />
+                </div>
+                <div>Frequency Bars</div>
+                <div class="mb-2 border-2 border-white">
+                    <PlaygroundAudioFrequencyBarGraph :canvas-height="250" :canvas-width="canvasWidth" />
+                </div>
+                <div>Spectrogram</div>
+                <div class="mb-2 border-2 border-white">
+                    <PlaygroundAudioSpectrogram :canvas-height="250" :canvas-width="canvasWidth" />
+                </div>
+            </div>
         </div>
-        <div>Frequency Bars</div>
-        <div class="mb-2 border-2 border-white">
-          <PlaygroundAudioFrequencyBarGraph :canvas-height="250" />
-        </div>
-        <div>Spectrogram</div>
-        <div class="mb-2 border-2 border-white">
-          <PlaygroundAudioSpectrogram :canvas-height="250" />
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
-<script>
+<script setup>
 definePageMeta({ layout: "light" });
-export default {
-  data() {
-    return {
-      dataArray: new Uint8Array(),
-      dataArrayHistory: [],
-    };
-  },
-  mounted() {
-    // TODO - use a single `dataArray` that is bound to the waveform, frequency, and spectrogram components. Still need
-    // to determine how to trigger the render callback on data update...
-    // navigator.mediaDevices
-    //   .getUserMedia({ audio: true })
-    //   .then((stream) => {
-    //     this.audioCtx = new window.AudioContext()
-    //     this.analyser = this.audioCtx.createAnalyser()
-    //     this.source = this.audioCtx.createMediaStreamSource(stream)
-    //     this.source.connect(this.analyser)
-    //     this.analyser.fftSize = 2048
-    //     this.bufferLength = this.analyser.frequencyBinCount
-    //     this.dataArray = new Uint8Array(this.bufferLength)
-    //     // repeatedly update time domain data
-    //     this.analyser_data_callback()
-    //   })
-    //   .catch(function (err) {
-    //     console.error(err)
-    //   })
-  },
-  methods: {
-    // analyser_data_callback: function () {
-    //   requestAnimationFrame(this.analyser_data_callback)
-    //   this.analyser.getByteTimeDomainData(this.dataArray)
-    //   // Vue is unable to detect updates to `this.dataArray` from
-    //   // `getByteTimeDomainData` causing reactivity issues -- we make a copy
-    //   // using `slice` to get around this.
-    //   // create copy via `.slice()` and push into history array
-    //   this.dataArrayHistory.push(this.dataArray.slice())
-    //   if (this.dataArrayHistory.length > 10) {
-    //     this.dataArrayHistory.shift()
-    //   }
-    // },
-  },
-};
+
+let canvasWidth = 750;
+
+// Hacky way to set canvas width to be appropriate on mobile. Should probably update to use
+// `canvas.clientWidth` like other experiments.
+if (window.innerWidth < 400) {
+    canvasWidth = 250;
+}
+
+// export default {
+//   data() {
+//     return {
+//       dataArray: new Uint8Array(),
+//       dataArrayHistory: [],
+//     };
+//   },
+//   mounted() {
+//     // TODO - use a single `dataArray` that is bound to the waveform, frequency, and spectrogram components. Still need
+//     // to determine how to trigger the render callback on data update...
+//     // navigator.mediaDevices
+//     //   .getUserMedia({ audio: true })
+//     //   .then((stream) => {
+//     //     this.audioCtx = new window.AudioContext()
+//     //     this.analyser = this.audioCtx.createAnalyser()
+//     //     this.source = this.audioCtx.createMediaStreamSource(stream)
+//     //     this.source.connect(this.analyser)
+//     //     this.analyser.fftSize = 2048
+//     //     this.bufferLength = this.analyser.frequencyBinCount
+//     //     this.dataArray = new Uint8Array(this.bufferLength)
+//     //     // repeatedly update time domain data
+//     //     this.analyser_data_callback()
+//     //   })
+//     //   .catch(function (err) {
+//     //     console.error(err)
+//     //   })
+//   },
+//   methods: {
+//     // analyser_data_callback: function () {
+//     //   requestAnimationFrame(this.analyser_data_callback)
+//     //   this.analyser.getByteTimeDomainData(this.dataArray)
+//     //   // Vue is unable to detect updates to `this.dataArray` from
+//     //   // `getByteTimeDomainData` causing reactivity issues -- we make a copy
+//     //   // using `slice` to get around this.
+//     //   // create copy via `.slice()` and push into history array
+//     //   this.dataArrayHistory.push(this.dataArray.slice())
+//     //   if (this.dataArrayHistory.length > 10) {
+//     //     this.dataArrayHistory.shift()
+//     //   }
+//     // },
+//   },
+// };
 </script>
