4949344e6b95bc3b8ff69177a22aecdec159df77
Colton Padden <colton.padden@fastmail.com>
2023-06-27 21:00:25 -0400

    experimenting with rendering arcs for spectrogram amplitudes

--- components/playground/audio/Spectrogram2.vue
+++ components/playground/audio/Spectrogram2.vue
@@ -23,6 +23,14 @@ const props = defineProps({
     },
 });
 
+const map = (n, nMin, nMax, rMin, rMax) => {
+    // normalize a number between ranges `nMin` to `nMax` to fall between
+    // range `rMin` and `rMax`.
+    return ((n - nMin) * (rMax - nMin)) / (nMax - nMin) + rMin;
+};
+
+const HISTORY_WINDOW_SIZE = 36;
+
 onMounted(() => {
     const canvas = document.getElementById("spectrogramCanvas");
     const canvasCtx = canvas.getContext("2d");
@@ -36,32 +44,62 @@ onMounted(() => {
         ? props.canvasWidth
         : container.clientWidth;
 
-    console.log(canvasHeight, canvasWidth)
-
     canvas.height = canvasHeight;
     canvas.width = canvasWidth;
+
+    canvasCtx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     watch(props.audioBufferHistory, (history) => {
         canvasCtx.fillStyle = props.fillStyle;
         canvasCtx.fillRect(0, 0, canvasWidth, canvasHeight);
+        const focusedHistory = history.slice(-HISTORY_WINDOW_SIZE);
 
-        const pixelWidth = canvasWidth / history[0].length;
-        const pixelHeight = canvasHeight / history.length;
+        const pixelWidth = canvasWidth / 128;
+        const pixelHeight = canvasHeight / HISTORY_WINDOW_SIZE;
 
-        // for each `y` of history, we increment along the height of the canvas
-        // for each `x` we increment along the width
-        for (let y = 0; y < history.length; y++) {
-            for (let x = 0; x < history[y].length; x++) {
-                const d = history[y][x];
-                canvasCtx.fillStyle = `rgb(${d / 2},${d},${d})`;
-                canvasCtx.fillRect(
-                    x * pixelWidth,
-                    y * pixelHeight,
-                    pixelWidth,
-                    pixelHeight
+        // create a circle for each sample point with varying size and color based on
+        // amplitude
+        for (let x = 0; x < 128; x++) {
+            for (let y = 0; y < HISTORY_WINDOW_SIZE; y++) {
+                const d = focusedHistory[y][x];
+                const r = map(d, 0, 255, 1, 4);
+                canvasCtx.fillStyle = `rgb(${d}, 0, ${d})`;
+                canvasCtx.beginPath();
+                canvasCtx.arc(
+                    x * pixelWidth + pixelWidth / 2,
+                    y * pixelHeight + pixelHeight / 2,
+                    r,
+                    0, // arc start
+                    2 * Math.PI // arc end
                 );
+                canvasCtx.fill();
             }
         }
     });
+
+    // legacy version which uses solid rectangles
+
+    // watch(props.audioBufferHistory, (history) => {
+    // canvasCtx.fillStyle = props.fillStyle;
+    // canvasCtx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // const pixelWidth = canvasWidth / history[0].length;
+    // const pixelHeight = canvasHeight / history.length;
+
+    // // for each `y` of history, we increment along the height of the canvas
+    // // for each `x` we increment along the width
+    // for (let y = 0; y < history.length; y++) {
+    //     for (let x = 0; x < history[y].length; x++) {
+    //         const d = history[y][x];
+    //         canvasCtx.fillStyle = `rgb(${d / 2},${d},${d})`;
+    //         canvasCtx.fillRect(
+    //             x * pixelWidth,
+    //             y * pixelHeight,
+    //             pixelWidth,
+    //             pixelHeight
+    //         );
+    //     }
+    // }
+    // });
 });
 </script>
