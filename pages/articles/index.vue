d237b9100ff532033e04f9565642077c852299d9
Colton Padden <colton.padden@fastmail.com>
2024-07-03 17:02:47 -0400

    feat: refactor articles index to script setup composition api

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,8 +1,95 @@
+<script setup>
+const { data: articles } = await useAsyncData("articles", () =>
+  queryContent()
+    .only([
+      "_id",
+      "_path",
+      "title",
+      "description",
+      "date",
+      "img",
+      "author",
+      "tags",
+      "categories",
+      "img",
+      "excerpt",
+      "summary",
+    ])
+    .sort({ date: -1 })
+    .find(),
+);
+
+console.log(articles.value);
+
+const selectedTags = ref([]);
+const selectedCategories = ref([]);
+
+const categories = computed(() => {
+  return [
+    ...new Set(
+      articles.value
+        .map((article) => article.categories)
+        .flat()
+        .sort(),
+    ),
+  ];
+});
+
+const tags = computed(() => {
+  return [
+    ...new Set(
+      articles.value
+        .map((article) => article.tags)
+        .flat()
+        .sort(),
+    ),
+  ];
+});
+
+const visibleArticles = computed(() => {
+  return articles.value.filter((article) => {
+    if (selectedCategories.value.length > 0) {
+      if (
+        !selectedCategories.value.every((c) => article.categories.includes(c))
+      ) {
+        return false;
+      }
+    }
+
+    if (selectedTags.value.length > 0) {
+      if (!selectedTags.value.every((t) => article.tags.includes(t))) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+});
+
+function toggleTag(tag) {
+  if (selectedTags.value.includes(tag)) {
+    selectedTags.value = selectedTags.value.filter((el) => el !== tag);
+  } else {
+    selectedTags.value.push(tag);
+  }
+}
+
+function toggleCategory(cat) {
+  if (selectedCategories.value.includes(cat)) {
+    selectedCategories.value = selectedCategories.value.filter(
+      (el) => el !== cat,
+    );
+  } else {
+    selectedCategories.value.push(cat);
+  }
+}
+</script>
+
 <template>
   <section class="container mx-auto">
     <div class="grid grid-cols-4 gap-4">
       <div class="col-span-4 lg:col-span-3">
-        <div v-for="article of visible_articles" :key="article._id">
+        <div v-for="article of visibleArticles" :key="article._id">
           <div class="rounded-xl bg-black/50 text-white shadow-xl">
             <div class="relative mb-2 h-full">
               <!-- heading -->
@@ -38,7 +125,7 @@
                   v-for="tag in article.tags"
                   :key="tag"
                   :class="{
-                    'bg-green-600': selected_tags.includes(tag),
+                    'bg-orange-500': selectedTags.includes(tag),
                   }"
                   class="mr-2 inline-block select-none bg-black px-3 py-1 text-sm text-orange-200"
                 >
@@ -57,11 +144,10 @@
             v-for="category in categories"
             :key="category"
             :class="{
-              'bg-green-600': selected_categories.includes(category),
-              'hover:bg-red-600': selected_categories.includes(category),
+              'bg-orange-500': selectedCategories.includes(category),
             }"
             class="block cursor-pointer select-none border-t border-black px-4 py-1 text-orange-200 hover:bg-black"
-            @click="toggle_category(category)"
+            @click="toggleCategory(category)"
             >{{ category }}</a
           >
         </div>
@@ -73,11 +159,10 @@
               v-for="(tag, ix) in tags"
               :key="ix"
               :class="{
-                'bg-green-600': selected_tags.includes(tag),
-                'hover:bg-red-600': selected_tags.includes(tag),
+                'bg-orange-500': selectedTags.includes(tag),
               }"
               class="mb-2 mr-2 inline-flex h-8 cursor-pointer select-none content-center justify-center bg-black px-3 py-2 text-sm leading-4 text-orange-200"
-              @click="toggle_tag(tag)"
+              @click="toggleTag(tag)"
             >
               <div>{{ tag }}</div>
             </span>
@@ -87,105 +172,3 @@
     </div>
   </section>
 </template>
-
-<script>
-export default {
-  async setup() {
-    const articles = await queryContent()
-      .only([
-        "_id",
-        "_path",
-        "title",
-        "description",
-        "date",
-        "img",
-        "author",
-        "tags",
-        "categories",
-        "img",
-        "excerpt",
-        "summary",
-      ])
-      .sort({ date: -1 })
-      .find();
-
-    // .limit(5)
-
-    return {
-      articles,
-    };
-  },
-  data() {
-    return {
-      selected_tags: [],
-      selected_categories: [],
-    };
-  },
-  computed: {
-    categories() {
-      // unique tags for all articles
-      return [
-        ...new Set(
-          this.articles
-            .map((article) => article.categories)
-            .flat()
-            .sort(),
-        ),
-      ];
-    },
-    tags() {
-      // unique tags for all articles
-      return [
-        ...new Set(
-          this.articles
-            .map((article) => article.tags)
-            .flat()
-            .sort(),
-        ),
-      ];
-    },
-    visible_articles() {
-      return this.articles.filter((article) => {
-        // article must contain _every_ category in selected categories
-        if (this.selected_categories.length > 0) {
-          if (
-            !this.selected_categories.every((c) =>
-              article.categories.includes(c),
-            )
-          ) {
-            return false;
-          }
-        }
-
-        // article must contain _every_ tag in selected tags
-        if (this.selected_tags.length > 0) {
-          if (!this.selected_tags.every((t) => article.tags.includes(t))) {
-            return false;
-          }
-        }
-
-        // if no filtering has occurred, return the article
-        return true;
-      });
-    },
-  },
-  methods: {
-    toggle_tag(tag) {
-      if (this.selected_tags.includes(tag)) {
-        this.selected_tags = this.selected_tags.filter((el) => el !== tag);
-      } else {
-        this.selected_tags.push(tag);
-      }
-    },
-    toggle_category(cat) {
-      if (this.selected_categories.includes(cat)) {
-        this.selected_categories = this.selected_categories.filter(
-          (el) => el !== cat,
-        );
-      } else {
-        this.selected_categories.push(cat);
-      }
-    },
-  },
-};
-</script>
