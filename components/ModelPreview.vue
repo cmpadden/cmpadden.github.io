844b1e899012f40b4773c33e1d6e2fe6d47945b3
Colton Padden <colton.padden@fastmail.com>
2024-12-31 01:17:36 -0500

    create ModelPreview component

---
+++ components/ModelPreview.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="canvas" class="h-full" />
+</template>
+
+<script setup>
+const { $p5 } = useNuxtApp();
+
+const props = defineProps({
+    path: String,
+});
+
+onMounted(() => {
+    const sketch = (s) => {
+        const c = document.getElementById("canvas");
+
+        const WIDTH = c.clientWidth;
+        const HEIGHT = c.clientHeight;
+
+        let model;
+        let angle;
+        let rotate = true;
+
+        s.preload = () => {
+            model = s.loadModel(props.path, true);
+        };
+
+        s.setup = () => {
+            s.createCanvas(WIDTH, HEIGHT, s.WEBGL);
+        };
+
+        s.mouseClicked = () => {
+            rotate = false; // disable object rotation on click
+        };
+
+        s.draw = () => {
+            s.background(200);
+            s.lights();
+
+            if (rotate) {
+                angle = s.frameCount * 0.005;
+            }
+            s.rotateX(angle);
+            s.rotateY(angle);
+            s.orbitControl();
+
+            s.model(model);
+        };
+    };
+
+    // eslint-disable-next-line no-new
+    new $p5(sketch, "canvas");
+});
+</script>
--- pages/stl.vue
+++ pages/stl.vue
@@ -1,55 +1,9 @@
+<script setup>
+definePageMeta({ layout: "light" });
+</script>
+
 <template>
-  <div class="">
-    <div id="canvas" class="h-96 border-2 border-white" />
+  <div class="h-96 border-2 border-white">
+    <ModelPreview path="models/mac_mini_macbook_stand.stl" />
   </div>
 </template>
-
-<script>
-definePageMeta({ layout: "light" });
-export default {
-  data() {
-    return {};
-  },
-  mounted() {
-    const sketch = (s) => {
-      const c = document.getElementById("canvas");
-
-      const WIDTH = c.clientWidth;
-      const HEIGHT = c.clientHeight;
-
-      let model;
-      let angle;
-      let rotate = true;
-
-      s.preload = () => {
-        model = s.loadModel("models/mac_mini_macbook_stand.stl", true);
-      };
-
-      s.setup = () => {
-        s.createCanvas(WIDTH, HEIGHT, s.WEBGL);
-      };
-
-      s.mouseClicked = () => {
-        rotate = false; // disable object rotation on click
-      };
-
-      s.draw = () => {
-        s.background(200);
-        s.lights();
-
-        if (rotate) {
-          angle = s.frameCount * 0.005;
-        }
-        s.rotateX(angle);
-        s.rotateY(angle);
-        s.orbitControl();
-
-        s.model(model);
-      };
-    };
-
-    // eslint-disable-next-line no-new
-    new this.$p5(sketch, "canvas");
-  },
-};
-</script>
