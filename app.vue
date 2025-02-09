a7cfd84ac7d4fcad406c97ae33c80443f24c2edc
Colton Padden <colton.padden@fastmail.com>
2025-02-09 15:54:31 -0500

    Adds og and twitter meta tags

--- app.vue
+++ app.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+const title = "Colton Padden's: Blog & Experiments";
+const description = "The blog, portfolio, and repository of experiments by Colton Padden.";
+const preview_image = "/images/og-index.png";
+
 useSeoMeta({
-  title: "Colton Padden",
-  ogTitle: "Colton Padden",
-  description:
-    "This is where you can find the blog, portfolio, and experiments of Colton Padden.",
-  ogDescription:
-    "This is where you can find the blog, portfolio, and experiments of Colton Padden.",
+  'title': title,
+  'description': description,
+  'og:title': title,
+  'og:description': description,
+  'og:image': preview_image,
+  'twitter:card': "summary_large_image",
+  'twitter:image': preview_image,
+  'twitter:title': title,
+  'twitter:description': description,
 });
 </script>
 
