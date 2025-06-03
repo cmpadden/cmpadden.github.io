eb2755aa1f5496d78da0e25a53ff616ffacea3da
Colton Padden <colton.padden@fastmail.com>
2025-06-03 19:53:19 -0400

    Remove workaround of external link on blog posts

--- components/section/BlogPosts.vue
+++ components/section/BlogPosts.vue
@@ -19,13 +19,7 @@ const props = defineProps({
 
       <div class="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
         <div v-for="(article, ix) of articles" :key="ix">
-          <!--
-            Specify `external` to force the link to be considered as external. This
-            resolves an issue where navigating to a blog post would load the previous
-            post if the user clicked the back button -- this unexpected behavior is
-            still not entirely understood, but at least this is a workaround.
-          -->
-          <NuxtLink :to="article.path" external>
+          <NuxtLink :to="article.path">
             <div
               class="h-full space-y-4 rounded-lg bg-white/20 p-4 text-white drop-shadow-lg hover:ring-1 hover:ring-white"
             >
